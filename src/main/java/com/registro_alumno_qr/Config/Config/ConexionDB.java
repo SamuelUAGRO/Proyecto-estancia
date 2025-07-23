093611ea0957c1fa5ffb645eb317288108bdfbfc
Samuel Antonio Ramos <saanraal.0610@gmail.com>
2025-07-22 18:57:17 -0600

    Creacion de packages en lugar de carpetas

---
+++ src/main/java/com/registro_alumno_qr/Config/Config/ConexionDB.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.registro_alumno_qr.Config.Config;
+
+/**
+ *
+ * @author samuel antonio
+ */
+public class ConexionDB {
+    
+}
