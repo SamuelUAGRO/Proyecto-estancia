7e5f3ef834cc60a768ec208bdb80c1dc8929dac2
Ariel <arckontre@gmail.com>
2025-07-31 12:45:17 -0600

    Git ignore prueba

--- src/main/java/com/registro_alumno_qr/Registro_alumno_qr.java
+++ src/main/java/com/registro_alumno_qr/Registro_alumno_qr.java
@@ -1,14 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.registro_alumno_qr;
 
-/**
- *
- * @author samuel antonio
- */
+
 public class Registro_alumno_qr {
 
     public static void main(String[] args) {
--- src/main/java/com/registro_alumno_qr/View/AgregarAlumno.java
+++ src/main/java/com/registro_alumno_qr/View/AgregarAlumno.java
@@ -26,17 +26,22 @@ public class AgregarAlumno extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        bg = new javax.swing.JPanel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        bg.setBackground(new java.awt.Color(255, 255, 255));
+        bg.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(bg, javax.swing.GroupLayout.DEFAULT_SIZE, 820, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(bg, javax.swing.GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE)
         );
 
         pack();
@@ -78,5 +83,6 @@ public class AgregarAlumno extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel bg;
     // End of variables declaration//GEN-END:variables
 }
