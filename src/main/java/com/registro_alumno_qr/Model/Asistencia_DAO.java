4530c343abc478225d353d4de327cabb633f0d85
Ariel <arckontre@gmail.com>
2025-08-05 21:20:18 -0600

    Cree el modelo Asistencia, al igual que su DAO

---
+++ src/main/java/com/registro_alumno_qr/Model/Asistencia_DAO.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.registro_alumno_qr.Model;
+
+/**
+ *
+ * @author betsy
+ */
+public class Asistencia_DAO {
+    
+}
